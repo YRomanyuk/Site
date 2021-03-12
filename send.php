9d04925d95538fcf1788c04f2e90fff6f5da6376
YRomanyuk <78836031+YRomanyuk@users.noreply.github.com>
2021-03-12 11:19:02 +0700

    Update send.php

--- send.php
+++ send.php
@@ -1,17 +1,21 @@
 <?php
+
+if(isset($_POST['submit'])){
+$to = "jromanyuk@ngs.ru";; // Здесь нужно написать e-mail, куда будут приходить письма
+$from = $_POST['email']; // this is the sender's Email address
 $first_name = $_POST['first_name'];
-$email = $_POST['email'];
-$first_name = htmlspecialchars($first_name);
-$email = htmlspecialchars($email);
-$first_name = urldecode($first_name);
-$email = urldecode($email);
-$first_name = trim($first_name);
-$email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("jromanyuk@ngs.ru", "Заявка с сайта", "ФИО:".$first_name.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
- {     echo "сообщение успешно отправлено";
-} else {
-    echo "при отправке сообщения возникли ошибки";
-}?>
+$subject =  $_POST['subject'];
+$subject2 = "Copy of your form submission";
+$message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+$message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+$headers = "From:" . $from;
+$headers2 = "From:" . $to;
+
+mail($to,$subject,$message,$headers);
+// mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
+echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+
+}
+
+?>
